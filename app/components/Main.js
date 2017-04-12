c6335ecb534a94ccfd163e5d90145e81b6a24f9f
Chiara DeLucia <chiaramdelucia@gmail.com>
2017-04-12 12:14:31 -0400

    Adding boilerplate for front-end routing.

---
+++ app/components/Main.js
@@ -0,0 +1,67 @@
+
+
+//EXAMPLE code of what the new React for ES6 looks like -- should follow this example
+import React from "react";
+import ReactDOM from "react-dom";
+import CounterDisplay from "./components/CounterDisplay";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    // Since we're passing these methods to be used as callback functions,
+    // we should bind them to our component here
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+  handleIncrement() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  handleDecrement() {
+    this.setState({ count: this.state.count + -1 });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-md-offset-4">
+            <div style={styles.panelStyle} className="panel panel-default">
+              <div className="panel-heading text-center">Clicky Count!</div>
+              <div className="panel-body text-center">
+                <CounterDisplay count={this.state.count} />
+                <button
+                  style={styles.buttonStyles}
+                  onClick={this.handleIncrement}
+                  className="btn btn-primary"
+                >
+                  Increment
+                </button>
+                <button
+                  style={styles.buttonStyles}
+                  onClick={this.handleDecrement}
+                  className="btn btn-danger"
+                >
+                  Decrement
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  buttonStyles: {
+    margin: 10
+  },
+  panelStyle: {
+    marginTop: 50
+  }
+};
+
+ReactDOM.render(
+  <App />, document.getElementById("app"));
---
+++ app/config/routes.js
@@ -0,0 +1,42 @@
+// Include the React library
+var React = require("react");
+
+// Include the react-router module
+var router = require("react-router");
+
+// Include the Route component for displaying individual routes
+var Route = router.Route;
+
+// Include the Router component to contain all our Routes
+// Here where we can pass in some configuration as props
+var Router = router.Router;
+
+// Include the hashHistory prop to handle routing client side without a server
+// https://github.com/ReactTraining/react-router/blob/master/docs/guides/Histories.md#hashhistory
+var hashHistory = router.hashHistory;
+
+// Include the IndexRoute (catch-all route)
+var IndexRoute = router.IndexRoute;
+
+// Reference the high-level components
+var Main = require("../components/Main");
+var *** = require("../components/*****************");
+
+
+// Export the Routes
+module.exports = (
+
+  // The high level component is the Router component
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+
+      {/* If user selects Info or Chat show the appropriate component */}
+      <Route path="**************" component={************} />
+
+      {/* If user selects any other path... we get the Info Route */}
+      <IndexRoute component={Main} />
+
+    </Route>
+  </Router>
+
+);
