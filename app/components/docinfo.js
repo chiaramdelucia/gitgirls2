3c3cd9d8aadf7500cbc153a2cafb320e651469e2
klawgolfer7 <kevinrlawler@gmail.com>
2017-04-18 21:14:27 -0400

    updated api information

--- app/components/docinfo.js
+++ app/components/docinfo.js
@@ -7,7 +7,7 @@ class App extends React.Component {
 		super();
 		// this.state would need to be whatever we need to put it at
 
-	}
+	};
 
 	const api_key = '4e6764b7fa2910445e23428d12cdc643';
 	// any functions that we want to add.  i.e changing the state
@@ -15,6 +15,11 @@ class App extends React.Component {
 	function doctorSearch() {
 		searchVal = $("This is tbd once the frontend is starting to get worked on").val().trim();
 		var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + api_key;
+		baseAjax = $.ajax({
+			url: resource_url,
+			method: 'GET',
+			dataType: 'jsonp'
+		}).done(function(response) )
 
 
 
