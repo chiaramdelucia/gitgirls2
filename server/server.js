c48ff5f04295921cfb854d59c73df11ae0f3b397
catherine savva <csoriasavva@gmail.com>
2017-04-29 12:23:48 -0400

    debuggin server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static("./public"));
+app.use(express.static(__dirname + '/public'));
 
 mongoose.Promise = Promise;
 
