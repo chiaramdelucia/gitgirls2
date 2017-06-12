5a57c4c0c31938c56c526003d9d7854f09ca4095
catherine savva <csoriasavva@gmail.com>
2017-06-12 14:12:02 -0400

    mock up of Account profile page

--- app/components/Account.js
+++ app/components/Account.js
@@ -11,15 +11,30 @@ export default class Account extends Component {
   		console.log("Account PROPS",this.props);
     	return (
     		<div className="signuppage">
-    			<div className='row'>
+    			 <div className='row'>
           			<div className="col-md-4">
                 		<button id="transparent"><Link to="/"><span ><img className='logoMain' src="./images/logo.png" alt="logo"/></span></Link></button>  
           			</div>
         		</div>	
-				<div>
+				    <div>
 	        		<h1>Account Profile</h1>
-	      		</div>
-      		</div>
+	           </div>
+             <div className="container profile">
+               <div className="col-md-4">
+                <img src="./images/profile_pic.png" alt="logo" />
+               </div>
+               <div className="col-md-8">
+                 <h3 className="profilehead">Username:</h3> <p className="profilep">superwoman17 </p>
+                 <br />
+                 <h3 className="profilehead">Condition: </h3><p className="profilep"> Ovarian</p>
+                 <br />
+                 <h3 className="profilehead">Relationship to Patient: </h3><p className="profilep">Self</p>
+                 <br /> 
+                 <h3 className="profilehead">About Me: </h3><p className="profilep">I am a comic book loving RN who can't stand how itchy her head is under these 'fashionable' head scarfs. Looking to talk to people who can relate to the trivial annoyances of the day to day life post-chemo.</p>
+               </div>
+             </div>
+
+      	</div>
     	)
   	}
 }
