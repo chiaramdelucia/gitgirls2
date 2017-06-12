e01356c7f32b832efbd488f6ece40d1137998b1f
catherine savva <csoriasavva@gmail.com>
2017-06-12 15:37:47 -0400

    styling profilerofile page

--- app/components/Account.js
+++ app/components/Account.js
@@ -11,29 +11,35 @@ export default class Account extends Component {
   		console.log("Account PROPS",this.props);
     	return (
     		<div className="signuppage">
-    			 <div className='row'>
-          			<div className="col-md-4">
-                		<button id="transparent"><Link to="/"><span ><img className='logoMain' src="./images/logo.png" alt="logo"/></span></Link></button>  
-          			</div>
-        		</div>	
-				    <div>
-	        		<h1>Account Profile</h1>
-	           </div>
+          <div className="container top">
+            <div className="row">  
+              <div className="col-md-4"></div>
+              <div className="col-md-4">
+                <h1>Account Profile</h1>
+              </div>
+              <div className="col-md-4">
+                <button id="transparent"><Link to="/"><span ><img className='logoMain' src="./images/logo.png" alt="logo"/></span></Link></button>  
+              </div>
+            </div>
+          </div>
+ <hr></hr>
              <div className="container profile">
                <div className="col-md-4">
                 <img src="./images/profile_pic.png" alt="logo" />
                </div>
                <div className="col-md-8">
-                 <h3 className="profilehead">Username:</h3> <p className="profilep">superwoman17 </p>
-                 <br />
-                 <h3 className="profilehead">Condition: </h3><p className="profilep"> Ovarian</p>
-                 <br />
-                 <h3 className="profilehead">Relationship to Patient: </h3><p className="profilep">Self</p>
-                 <br /> 
-                 <h3 className="profilehead">About Me: </h3><p className="profilep">I am a comic book loving RN who can't stand how itchy her head is under these 'fashionable' head scarfs. Looking to talk to people who can relate to the trivial annoyances of the day to day life post-chemo.</p>
+                 <dl className="dl-horizontal">
+                  <dt>Username</dt>
+                  <dd>superwoman17</dd>
+                  <dt>Condition</dt>
+                  <dd>Ovarian</dd>
+                  <dt>Relationship to Patient</dt>
+                  <dd>Self</dd>
+                  <dt>About Me</dt>
+                  <dd>I am a comic book loving RN who can't stand how itchy her head is under these 'fashionable' head scarfs. Looking to talk to people who can relate to the trivial annoyances of the day-to-day life post-chemo.</dd>
+                 </dl>
                </div>
              </div>
-
       	</div>
     	)
   	}
